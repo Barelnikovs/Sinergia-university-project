92c4ed1cb9f8fa440cbd9b18b96157f1d3955b79
Barelnikov Daniil <daniilbarelnikov@gmail.com>
2025-04-16 18:14:31 +0300

    refactor: add all texts from main page

---
+++ src/content/buttonsAndInputs.js
@@ -0,0 +1,27 @@
+export default {
+    studyProgram: 'Программы обучения',
+    getConsultation: 'Получить консультацию',
+    getMasterClasses: 'Получить мастер-классы',
+    leaveRequest: 'Оставить заявку',
+    BookPlace: 'Забронировать место',
+    trustProfessional: 'Довериться профессионалу',
+    askYourQuestion: 'Задать свой вопрос',
+    follow: 'Подписаться',
+    programSelection: {
+        college: 'Колледж',
+        bachelor: 'Бакалавр',
+        master: 'Магистратура',
+        courses: 'Курсы',
+        more: 'Подробнее'
+    },
+    supportOfStudent: {
+        centerOfCareer: 'Центр карьеры',
+        practice: 'Практика и проекты',
+        community: 'Коммьюнити'
+    },
+    placeholders: {
+        name: 'Имя',
+        tel: 'Телефон',
+        email: 'E-mail'
+    },
+}
